007fababbe7684eb253459ca69f4e1e8347d029b
danu <danujagreru@gmail.com>
2023-08-30 18:04:22 +0530

    navigate Customer Form with creating a new Stage

---
+++ src/main/java/lk/ijse/javafxtutorial/controller/CustomerFormController.java
@@ -0,0 +1,9 @@
+package lk.ijse.javafxtutorial.controller;
+
+/*
+    @author DanujaV
+    @created 8/30/23 - 5:52 PM   
+*/
+
+public class CustomerFormController {
+}
--- src/main/java/lk/ijse/javafxtutorial/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/javafxtutorial/controller/DashboardFormController.java
@@ -5,5 +5,29 @@ package lk.ijse.javafxtutorial.controller;
     @created 8/30/23 - 5:09 PM   
 */
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) throws IOException {
+        // open Customer Form
+
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/customer_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = new Stage();
+        stage.setTitle("Customer Form");
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
