f4126a6375c85bee965095528b56c952af869a99
danu <danujagreru@gmail.com>
2023-08-30 17:34:36 +0530

    Launcher Vs. LauncherWrapper

--- src/main/java/lk/ijse/javafxtutorial/Launcher.java
+++ src/main/java/lk/ijse/javafxtutorial/Launcher.java
@@ -5,5 +5,27 @@ package lk.ijse.javafxtutorial;
     @created 8/30/23 - 5:05 PM   
 */
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/dashboard_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        stage.setTitle("Dashboard");
+        stage.centerOnScreen();
+        stage.setScene(scene);
+
+
+        stage.show();
+    }
 }
---
+++ src/main/java/lk/ijse/javafxtutorial/LauncherWrapper.java
@@ -0,0 +1,12 @@
+package lk.ijse.javafxtutorial;
+
+/*
+    @author DanujaV
+    @created 8/30/23 - 5:33 PM   
+*/
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
